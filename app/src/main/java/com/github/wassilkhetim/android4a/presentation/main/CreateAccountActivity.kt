b85deb9f5b14ca170e78eefef749bf9e0f06ee16
Wassangota <khetim.wassil@gmail.com>
2020-12-29 13:10:40 +0100

    Ajout activity création de compte

---
+++ app/src/main/java/com/github/wassilkhetim/android4a/presentation/main/CreateAccountActivity.kt
@@ -0,0 +1,12 @@
+package com.github.wassilkhetim.android4a.presentation.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.wassilkhetim.android4a.R
+
+class CreateAccountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+    }
+}
